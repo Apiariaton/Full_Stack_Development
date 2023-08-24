e05001470177ed5517732ec1fb4a406b3df9242b
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2023-08-24 22:33:06 +0100

    Create map.js

---
+++ React.js/Introduction_to_React/Javascript_Refresher/map.js
@@ -0,0 +1,5 @@
+const MutantHeroNinjaTurtles = ["Michaelangelo","Donatello","Raphael","Leonardo"];
+const editedMutantHeroNinjaTurtles = MutantHeroNinjaTurtles.map((item)=>{return item + "..."});
+
+const Characters = ["Mario","Luigi","Ash"];
+const editedCharacters = Characters.map((item)=> return ( { name : item, popularity : 50 }   )}; 
