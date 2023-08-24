7281c34bdd0a0f8b131337ecbfa5e7bb2c66284b
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2023-08-24 22:36:17 +0100

    Update map.js

--- React.js/Introduction_to_React/Javascript_Refresher/map.js
+++ React.js/Introduction_to_React/Javascript_Refresher/map.js
@@ -3,3 +3,10 @@ const editedMutantHeroNinjaTurtles = MutantHeroNinjaTurtles.map((item)=>{return 
 
 const Characters = ["Mario","Luigi","Ash"];
 const editedCharacters = Characters.map((item)=> return ( { name : item, popularity : 50 }   )}; 
+
+function transformToObjects(numberArray) {
+    // Todo: Add your logic
+    return const Array_of_Objects = numberArray.map((item)=>{({val:item})});
+    // should return an array of objects
+}
+
