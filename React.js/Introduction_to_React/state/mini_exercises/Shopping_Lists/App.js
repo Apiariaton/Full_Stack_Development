e2ca916cf7508ecc2555de5cc0c75b6e107ce6d1
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2023-08-31 14:12:51 +0100

    Update App.js

--- React.js/Introduction_to_React/state/mini_exercises/Shopping_Lists/App.js
+++ React.js/Introduction_to_React/state/mini_exercises/Shopping_Lists/App.js
@@ -1,30 +1,10 @@
-import Expenses from "./components/Expenses/Expenses";
-import CardNoBs from "./components/UI/CardNoBs";
-import NewExpense from "./components/NewExpenses/NewExpense";
-import MonthlyShoppingList from "./components/Misc/MonthlyShoppingList";
-import ShoppingList from "./components/Misc/ShoppingList";
+import MonthlyShoppingList from "./MonthlyShoppingList";
+import ShoppingList from "./ShoppingList";
 
 function App() {
 
-  const expenses = [
-    {
-      id: 0,
-      title: "Car Insurance",
-      price: 677,
-      date: new Date(2021, 3, 2023),
-    },
-
-    { id: 1, title: "Groceries", price: 80, date: new Date(2021, 3, 2023) },
-
-    { id: 2, title: "Book", price: 8, date: new Date(2021, 3, 2023) },
-  ];
-
   return (
-    <div className="expenses">
-      <NewExpense/>
-      <CardNoBs>
-      <Expenses expenses={expenses}></Expenses>
-      </CardNoBs>
+    <div>
       <MonthlyShoppingList></MonthlyShoppingList>
     </div>
   );
