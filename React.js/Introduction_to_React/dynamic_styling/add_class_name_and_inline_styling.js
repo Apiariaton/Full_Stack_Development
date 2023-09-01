10cca0ecf98f90866b4de0eac09942db01530052
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2023-09-01 17:22:20 +0100

    Create add_class_name_and_inline_styling.js

---
+++ React.js/Introduction_to_React/dynamic_styling/add_class_name_and_inline_styling.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const [isValidGoal, setValidGoal] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    setValidGoal(true);
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setValidGoal(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue("");
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValidGoal ? "invalid" : ""}`}>
+        <label>
+          Course Goal
+        </label>
+        <input
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
