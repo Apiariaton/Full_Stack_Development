9e80b9b12ad3cd5b87e6ad9d90cccf2a33381cc7
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2023-09-02 10:51:28 +0100

    Update MainBankAccount.java

--- Java/Introduction_to_Java/CalculateScore/src/MainBankAccount.java
+++ Java/Introduction_to_Java/CalculateScore/src/MainBankAccount.java
@@ -12,6 +12,25 @@ public class MainBankAccount {
                        "\n Customer Account Balance : £" + tomsNewBankAccount.getAccountBalance()
     );
 
+    BankAccount bobsNewBankAccount = new BankAccount(654321,400.00,"Bob McIllroy", "bob@gmail.com",0112345543);
+
+        System.out.println("\n Customer Name :" + bobsNewBankAccount.getCustomerName() +
+                "\n Customer Email :" + bobsNewBankAccount.getCustomerEmail() +
+                "\n Customer Phone Number :" + bobsNewBankAccount.getCustomerPhoneNumber() +
+                "\n Customer Account Number :" + bobsNewBankAccount.getAccountNumber() +
+                "\n Customer Account Balance : £" + bobsNewBankAccount.getAccountBalance()
+        );
+        
+    BankAccount marysNewBankAccount = new BankAccount("Mary","mary@gmail.com",1000000);
+
+        System.out.println("\n Customer Name :" + marysNewBankAccount.getCustomerName() +
+                "\n Customer Email :" + marysNewBankAccount.getCustomerEmail() +
+                "\n Customer Phone Number :" + marysNewBankAccount.getCustomerPhoneNumber() +
+                "\n Customer Account Number :" + marysNewBankAccount.getAccountNumber() +
+                "\n Customer Account Balance : £" + marysNewBankAccount.getAccountBalance()
+        );
+    
+    
     tomsNewBankAccount.setCustomerName("Tom Nosmot");
     tomsNewBankAccount.setAccountNumber(12345678);
     tomsNewBankAccount.setCustomerEmail("tom_nos@gmail.com");
